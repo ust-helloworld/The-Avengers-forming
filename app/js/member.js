8cc6902933a5a58b347fd48acb41834da46147ba
wai2016 <jackietong2014@gmail.com>
2016-11-11 18:26:49 +0800

    Modify member.html and member.js (#28)
    * Set default value for user name/id when logged in
    
    * Fix the spacing
    
    * Set default value for user name/id when logged in
    
    * Fix the spacing
    
    * Fix syntax error and spacing
    
    * Delete a useless button
    
    * Modify member.js to load the data

--- app/js/member.js
+++ app/js/member.js
@@ -7,41 +7,42 @@ $(document).ready(function(){
 		$('#text_event_name').text("Event name: " + eventName);
 		$('#member_page_controller').show();
 	}
-
 });
 
 angular.module('teamform-member-app', ['firebase'])
 .controller('MemberCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
-	
 	// TODO: implementation of MemberCtrl
-	
-	
+
 	// Call Firebase initialization code defined in site.js
 	if (firebase.apps.length === 0)
 	{
-	initalizeFirebase();
+		initalizeFirebase();
 	}
-	
-	$scope.userID = "";
-	$scope.userName = "";	
+
+	// Set default member to be the current user
+	firebase.auth().onAuthStateChanged(function(user){
+		if (user) {
+			$scope.userID = user.uid;
+			$scope.userName = user.displayName;
+			$scope.loadFunc();
+		} else {
+			$scope.userID = "Please log in";
+			$scope.userName = "Please log in";
+			alert("Please log in");
+		}
+	});
 	$scope.teams = {};
-	
-	
-	
+
 	$scope.loadFunc = function() {
 		var userID = $scope.userID;
 		if ( userID !== '' ) {
-			
 			var refPath = getURLParameter("q") + "/member/" + userID;
 			retrieveOnceFirebase(firebase, refPath, function(data) {
-								
 				if ( data.child("name").val() != null ) {
 					$scope.userName = data.child("name").val();
 				} else {
 					$scope.userName = "";
 				}
-				
-				
 				if (data.child("selection").val() != null ) {
 					$scope.selection = data.child("selection").val();
 				}
@@ -52,45 +53,34 @@ angular.module('teamform-member-app', ['firebase'])
 			});
 		}
 	}
-	
+
 	$scope.saveFunc = function() {
-		
-		
 		var userID = $.trim( $scope.userID );
 		var userName = $.trim( $scope.userName );
-		
 		if ( userID !== '' && userName !== '' ) {
-									
-			var newData = {				
+			var newData = {
 				'name': userName,
 				'selection': $scope.selection
 			};
-			
-			var refPath = getURLParameter("q") + "/member/" + userID;	
+			var refPath = getURLParameter("q") + "/member/" + userID;
 			var ref = firebase.database().ref(refPath);
-			
 			ref.set(newData, function(){
 				// complete call back
 				//alert("data pushed...");
-				
+
 				// Finally, go back to the front-end
 				window.location.href= "index.html";
 			});
-			
-			
-		
-					
 		}
 	}
-	
+
 	$scope.refreshTeams = function() {
-		var refPath = getURLParameter("q") + "/team";	
+		var refPath = getURLParameter("q") + "/team";
 		var ref = firebase.database().ref(refPath);
-		
 		// Link and sync a firebase object
-		$scope.selection = [];		
+		$scope.selection = [];
 		$scope.toggleSelection = function (item) {
-			var idx = $scope.selection.indexOf(item);    
+			var idx = $scope.selection.indexOf(item);
 			if (idx > -1) {
 				$scope.selection.splice(idx, 1);
 			}
@@ -98,24 +88,15 @@ angular.module('teamform-member-app', ['firebase'])
 				$scope.selection.push(item);
 			}
 		}
-	
-	
 		$scope.teams = $firebaseArray(ref);
 		$scope.teams.$loaded()
 			.then( function(data) {
-								
-							
-							
-			}) 
+
+			})
 			.catch(function(error) {
 				// Database connection error handling...
 				//console.error("Error:", error);
 			});
-			
-		
 	}
-	
-	
 	$scope.refreshTeams(); // call to refresh teams...
-		
 }]);
