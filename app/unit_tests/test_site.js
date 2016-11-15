2bc885255bdd0a9b412306acbcd7d3477462ca56
RaymondWongCH <raymondchwong@gmail.com>
2016-11-15 19:40:31 +0800

    Modify test cases (#48)

--- app/unit_tests/test_site.js
+++ app/unit_tests/test_site.js
@@ -3,42 +3,29 @@ describe('Test site.js', function() {
     //
     // Example: A test case of getRandomIntInclusive
     //
-
-
-	    it('value within 1 to 3', function() {
-            var value = getRandomIntInclusive(1, 3);
-            expect( value>=1 && value <= 3 ).toEqual(true);
-	    });
-
-
+    it('value within 1 to 3', function() {
+        var value = getRandomIntInclusive(1, 3);
+        expect( value>=1 && value <= 3 ).toEqual(true);
+    });
 
     //
     // Raymond's work
     //
- 
-
-        it('have firebase', function() {
-            if (firebase.apps.length === 0)
-            {
+    it('have firebase', function() {
+        if (firebase.apps.length === 0)
+        {
             initalizeFirebase();
-            }
-            expect( firebase ).toBeDefined();
-        });
+        }
+        expect( firebase ).toBeDefined();
+    });
 
+    it('testing value', function() {
+        var test = getURLParameter('testing');
+        expect( test ).toEqual(decodeURIComponent((new RegExp('[?|&]' + 'testing' + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null);
+    });
 
-
-        it('testing value', function() {
-            var test = getURLParameter('testing');
-            expect( test ).toEqual(decodeURIComponent((new RegExp('[?|&]' + 'testing' + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null);
-        });
-
-
-
-        it('retrieve Firebase', function() {
-            expect( retrieveOnceFirebase(firebase, "/", null) ).toBeUndefined();
-        });
-
-
-
+    it('retrieve Firebase', function() {
+        expect( retrieveOnceFirebase(firebase, "/", null) ).toBeUndefined();
+    });
 
 });
