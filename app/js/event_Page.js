cf7cf3cfaea0e9b86554439e2811486b86c07d59
p-wong1124 <kwwongax@ust.hk>
2016-11-24 20:42:52 +0800

    Added some Angular to event.html
    - Able to display eventName, team and teamOwner. OwnerName get by retrieved by user file :)
    
    - Use event.html?q= ... to go to event
    
    - Put a temporatory button for send requests to group <----Help me to implement thx :)

---
+++ app/js/event_Page.js
@@ -0,0 +1,72 @@
+angular.module('teamform-event-app', ['firebase'])
+.controller('displayCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
+	// TODO: implementation of AdminCtrl
+
+	// Initialize $scope.param as an empty JSON object
+	$scope.param = {};
+
+	// Call Firebase initialization code defined in site.js
+	if (firebase.apps.length === 0)
+	{
+	  initalizeFirebase();
+	}
+
+
+	var refPath, ref, eventName;
+
+	eventName = getURLParameter("q");
+	$scope.eventName = eventName;
+	refPath = "event/" + eventName + "/admin/param";
+	ref = firebase.database().ref(refPath);
+
+	// Link and sync a firebase object
+
+	$scope.param = $firebaseObject(ref);
+	$scope.param.$loaded()
+		.then( function(data) {
+			// Fill in some initial values when the DB entry doesn't exist
+			if (typeof $scope.param.owner != "undefined")
+			{
+			  if ( $scope.param.owner != firebase.auth().currentUser.uid)
+			  {
+		        console.log("You are not admin!!! View only");
+			  }
+			}
+			
+			refPath = "/user/"+$scope.param.owner;
+			profile = $firebaseObject(firebase.database().ref(refPath));
+			profile.$loaded().then(function (data){
+				$scope.ownerDisplayName = profile.name;
+			});
+			
+		})
+		.catch(function(error) {
+			// Database connection error handling...
+			//console.error("Error:", error);
+		});
+	
+
+	refPath = "event/" + eventName + "/team";
+	$scope.team = [];
+	$scope.team = $firebaseArray(firebase.database().ref(refPath));
+
+
+	refPath = "event/" + eventName + "/member";
+	$scope.member = [];
+	$scope.member = $firebaseArray(firebase.database().ref(refPath));
+	
+	//still might not tested.
+	$scope.sendRequest = function (targetTeam){
+		refPath = "event/" + eventName + "/" + uid;
+		memberDetail = $firebaseObject(firebase.database().ref(refPath));
+		memberDetail.loaded().then(function (data){
+			if (typeof memberDetail.selection == "undefined")
+			{
+				memberDetail.selection = [];
+			}
+			memberDetail.selection.append(targetTeam);
+			console.log(memberDetail.selection);
+		});
+	}
+	
+}]);
