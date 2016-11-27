b60b96141cc798ec774bca58fa24ebfbe193bc7c
p-wong1124 <kwwongax@ust.hk>
2016-11-27 20:49:06 +0800

    Added a join button and the join team button will be appear after member has joined the event (#83)
    - use angular to control the show/block of the join button key.
    
    - Join button will become leave button after user has joined the event.
    
    - Once the event removed, the selection user had made will be flushed.

--- app/js/event_Page.js
+++ app/js/event_Page.js
@@ -11,6 +11,8 @@ app.controller('displayCtrl', ['$scope', '$firebaseObject', '$firebaseArray', fu
 	  initalizeFirebase();
 	}
 
+	$scope.joined = false;
+	
 
 	var refPath, ref, eventName;
 
@@ -47,9 +49,30 @@ app.controller('displayCtrl', ['$scope', '$firebaseObject', '$firebaseArray', fu
 	
 
 	refPath = "event/" + eventName + "/team";
-	$scope.team = [];
 	$scope.team = $firebaseArray(firebase.database().ref(refPath));
-
+	
+	
+	/*display = function (inputData){
+		$.each(inputData,function(i, data){
+			console.log(data);
+			insertData = '<div class="col-md-4 text-center" id="team-'+data.$id+'" ng-controller = "displayCtrl">'+
+						'<h3 class="text-center"><a href="team.html?e='+eventName+'&q='+data.$id+'">'+data.$id+'</a></h3>'+
+						'<a class="thumbnail" href="team.html?e='+eventName+'+&q='+data.$id+'"> <img src="img/team.jpg"></a><br>'+
+						'Preferred team size: <span class="badge">'+data.size+'</span> <br>'+
+						'Current teamMember: <span class = "badge">'+data.teamMembers.length+'</span><br>'+
+						'Team Leader: '+data.leader+'<br>'+
+						'<p class="text-center">your position</p>'+
+						'<button type="button" class="btn btn-default btn-sendRequest" ng-click = "sendRequest(\''+data.$id+'\');" id="button-"'+data.$id+'" '+($scope.joined?'':'style="display:none"')+'>Request</button>'+
+						'<br><br><br></div>';
+			if (typeof ($('div#team-'+data.$id).get(0)) =='undefined'){
+				$("div#teamInsert").append(insertData);
+			}
+		});
+	};
+	$scope.team.$loaded().then(display);*/
+	
+	
+	
 
 	refPath = "event/" + eventName + "/member/";
 	$scope.member = [];
@@ -61,11 +84,18 @@ app.controller('displayCtrl', ['$scope', '$firebaseObject', '$firebaseArray', fu
 		refPath = "event/" + eventName + "/member/" + user.uid;
 		memberDetail = $firebaseObject(firebase.database().ref(refPath));
 		memberDetail.$loaded().then(function (data){
-		$.each(memberDetail.selection, function (index, teamSelection){
-			console.log(teamSelection);
-			$("#button-"+teamSelection).text ("Cancel request");
-			//still have some problems (the tag cannot be change to cancel request =v="")
-		});
+			console.log(memberDetail.name +" "+typeof memberDetail.name);
+			if (typeof memberDetail.name!="undefined"){
+				$('#joinButton').text("Leave the event");
+				$scope.joined = true;
+				$(".btn-sendRequest").show();
+			}
+			
+			$.each(memberDetail.selection, function (index, teamSelection){
+				console.log(teamSelection);
+				$("#button-"+teamSelection).text ("Cancel request");
+				//still have some problems (the tag cannot be change to cancel request =v="")
+			});
 	});
 	  } else {
 		// No user is signed in.
@@ -76,6 +106,7 @@ app.controller('displayCtrl', ['$scope', '$firebaseObject', '$firebaseArray', fu
 	//Cannot display the current state of sending request once user enter.
 	$scope.sendRequest = function (targetTeam){
 		console.log(targetTeam);
+
 		refPath = "event/" + eventName + "/member/" + firebase.auth().currentUser.uid;
 		var memberDetail = $firebaseObject(firebase.database().ref(refPath));
 		memberDetail.$loaded().then(function (data){
@@ -90,7 +121,7 @@ app.controller('displayCtrl', ['$scope', '$firebaseObject', '$firebaseArray', fu
 			$("#button-"+targetTeam).text ("Cancel request");
 			}
 			else {
-				alert("You have sent this!!!");
+				//alert("You have sent this!!!");
 				memberDetail.selection = $.grep(memberDetail.selection, function(delData){
 					return delData != targetTeam;
 				});
@@ -99,6 +130,27 @@ app.controller('displayCtrl', ['$scope', '$firebaseObject', '$firebaseArray', fu
 			}
 		});
 	}
+	
+	$scope.joinQuit = function(){
+		refPath = "event/" + eventName + "/member/" + firebase.auth().currentUser.uid;
+		var memberDetail = $firebaseObject(firebase.database().ref(refPath));
+		if ($scope.joined == false){
+			memberDetail.name = firebase.auth().currentUser.displayName;
+			memberDetail.$save();
+			$('#joinButton').text("Leave the event");
+			$scope.joined = true;
+			$('.btn-sendRequest').show();
+			$('.btn-sendRequest').text("Request");
+		}
+		else {
+			memberDetail.name = null;
+			memberDetail.selection = null;
+			memberDetail.$save();
+			$('#joinButton').text("Join the event");
+			$scope.joined = false;
+			$('.btn-sendRequest').hide();
+		}
+	};
 	
 }]);
 app.controller('CEA_Form', ['$scope', '$firebaseObject', '$firebaseArray', function CEA_Form($scope, $firebaseObject, $firebaseArray) {
