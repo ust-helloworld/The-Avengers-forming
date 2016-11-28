18b3e5978852bfbff5f910b6b43083ef2236815b
RaymondWongCH <raymondchwong@gmail.com>
2016-11-28 23:58:32 +0800

    Modify profile and create event

--- app/js/index.js
+++ app/js/index.js
@@ -64,6 +64,14 @@ angular.module('indexApp', ['firebase'])
             var refPath = "/user/" + $.trim( $scope.userdata.userID ) + "/skills";
             $scope.skillList = [];
             $scope.skillList = $firebaseArray(firebase.database().ref(refPath));
+            $scope.param = {
+                minTeamSize : 4,
+                maxTeamSize : 10,
+                description : "",
+                imgURL : "",
+                owner : $scope.userdata.userID,
+                requiredSkill : []
+            };
 
         } else {
             window.location.href= "login.html";
@@ -139,9 +147,9 @@ angular.module('indexApp', ['firebase'])
         skillName = skillName.toUpperCase();
         if ( userID !== '' && skillName !=='' && $scope.userdata.skills[skillName] == null) {
                 var newData = {
-                    'agree' : "0",
-                    'total' : "0",
-                    'percent' : "--"
+                    'agree' : 0,
+                    'total' : 0,
+                    'percent' : 0
 
                 };
                 $scope.userdata.skills[skillName] = newData;
@@ -160,28 +168,121 @@ angular.module('indexApp', ['firebase'])
 
     }
     $scope.removeskill = function(t) {
-        //var skillName = $.trim( t );
-        delete $scope.userdata.skills[t.$id];
-        var userID = $.trim( $scope.userdata.userID );
-    //var userName = $.trim( $scope.userName );
+            //var skillName = $.trim( t );
+        if (confirm("Are you sure to delete this skill?") == true) {
+            delete $scope.userdata.skills[t.$id];
+            var userID = $.trim( $scope.userdata.userID );
+            //var userName = $.trim( $scope.userName );
 
-        if ( userID !== '') {
-            var newData = {
-                'skills': $scope.userdata.skills
-            };
-            var refPath = "/user/" + userID;
-            var ref = firebase.database().ref(refPath);
-            ref.update(newData, function(){
-                // Complete call back
-                //alert("data pushed...");
+            if ( userID !== '') {
+                var newData = {
+                    'skills': $scope.userdata.skills
+                };
+                var refPath = "/user/" + userID;
+                var ref = firebase.database().ref(refPath);
+                ref.update(newData, function(){
+                    // Complete call back
+                    //alert("data pushed...");
 
-                // Finally, go back to the front-end
-                //window.location.href= "index.html";
-                console.log("Save data")
-            });
+                    // Finally, go back to the front-end
+                    //window.location.href= "index.html";
+                    console.log("Save data")
+                });
 
+            }
         }
 
+    }
+
+    var refPath, ref, eventName;
+
+    $scope.eventName = "";
+    $scope.requiredSkillname = "";
+    
+    
+    $scope.changeMinTeamSize = function(delta) {
+        var newVal = $scope.param.minTeamSize + delta;
+        if (newVal >=1 && newVal <= $scope.param.maxTeamSize ) {
+            $scope.param.minTeamSize = newVal;
+        } 
+    }
+
+    $scope.changeMaxTeamSize = function(delta) {
+        var newVal = $scope.param.maxTeamSize + delta;
+        if (newVal >=1 && newVal >= $scope.param.minTeamSize ) {
+            $scope.param.maxTeamSize = newVal;
+        } 
+    }
+    $scope.imgUpload = function(){
+        var database = firebase.database();
+        //database.ref(refPath).update({'description':$scope.FDescription});
+        
+        eventName = $scope.eventName;
+        
+        if (typeof document.getElementById("js-upload-files").files[0]!="undefined")
+      {
+          files = document.getElementById("js-upload-files").files;
+          console.log("have sth to upload");
+          console.log(files[0].name);
+
+          // Create a root reference
+          var storageRef = firebase.storage().ref();
+
+          // Create a reference to 'mountains.jpg'
+          var eventRef = storageRef.child("event/"+eventName);
+          eventRef.put(files[0]).then(function(snapshot){
+             console.log('Uploaded a file!');
+              var downloadURL = snapshot.downloadURL;
+              $scope.param.imgURL = downloadURL;
+          });
+      }
+    }
+
+    $scope.addrequiredSkill = function() {
+        var Add = $scope.requiredSkillname.toUpperCase();
+        if ($scope.requiredSkillname !== "" && $scope.param.requiredSkill.indexOf(Add) < 0) {
+            $scope.param.requiredSkill.push(Add);
+            //$scope.saveFunc();
+        }
+        
+    }
+
+    $scope.removerequiredSkill = function(t) {
+        var index = $scope.param.requiredSkill.indexOf(t);
+        if ( index > -1 ) {
+            $scope.param.requiredSkill.splice(index, 1); // remove that item
+            //$scope.saveFunc();
+        }
+        
+    }
+
+    $scope.saveFunc = function() {
+        var checkPath = "/event";
+        $scope.checkList = [];
+        $scope.checkList = $firebaseArray(firebase.database().ref(checkPath));
+        var new_name = true;
+        var name = $scope.eventName;
+        var newdata = $scope.param;
+        $scope.checkList.$loaded().then(function(data) {
+            for (i = 0; i < $scope.checkList.length; i++)
+            {
+                if ($scope.checkList[i].$id == name)
+                {
+                    new_name = false;
+                }
+            }
+            if (name !== "" && new_name)
+            {
+                console.log("click saved!!");
+                console.log(newdata);
+                refPath = "/event/"+name+"/admin/param";
+                firebase.database().ref(refPath).update(newdata);
+            }
+            else
+            {
+                alert("Invalid input or Event name already exists!")
+            }
+        })
     }
 
 }]);
