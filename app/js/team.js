4994da00c62ffc7c0920e762df939f4ecab22285
robbieyang <robbieyang1996@gmail.com>
2016-11-29 23:05:15 +0800

    FIXED ! (#102)
    * Fixed add member function
    
    * Rebuild the logic of team management
    
    * Updated member.html
    
    To handle invitation from team
    
    * Update member.js
    
    Handle invitation from team
    
    * updated team.html
    
    * Bug Fixed
    
    Fixed joinedTeam.
    
    * Deletion of CONSOLE.LOG
    
    * Some bugs fixed
    
    * COMMIT
    
    * Revert "Some bugs fixed"
    
    This reverts commit ec0922e1ceed7cdc1533ee9e3373a67b8b32e729.
    
    * Revert "Deletion of CONSOLE.LOG"
    
    This reverts commit 6b3ac28646f0bba7b8b188631c41facc9662f58c.
    
    * Merge UI to team.html(for leader),  page for member is under developing
    
    * small errors
    
    * New version released
    
    Functions is added
    
    * New update
    
    team function updates
    
    * TEAM PAGE BETA 2.0
    
    - bug fixed and member image is updated
    - store user id to teamMembers instead of store username
    
    * Revert "TEAM PAGE BETA 2.0"
    
    This reverts commit 04dc3e310f761c154e1034ee103454e091c2f533.
    
    * New updates
    
    * team beta 3.0
    
    * UPDATE 3.0
    
    * New updates
    
    ss
    
    * updates
    
    * BUG FIXED
    
    * update

--- app/js/team.js
+++ app/js/team.js
@@ -369,7 +369,8 @@ angular.module('teamform-team-app', ['firebase'])
 	}
 	$scope.joinQuit = function(){
 		firebase.auth().onAuthStateChanged(function(user){
-		var refP = "/event/" + eventName + "/team/" + user.uid;
+		var refP = "/event/" + eventName + "/member/" + user.uid;
+
 		console.log(refP);
 		retrieveOnceFirebase(firebase, refP, function(data) {
 			if(data.child("joinedTeam").val() == $scope.param.teamName ){
